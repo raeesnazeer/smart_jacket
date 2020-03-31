6dfdf767d941b798678df7ccdb862da709593cd8
Your Name <raeesnazeer@gmail.com>
2020-03-31 19:26:46 +0530

    Updated UI

--- lib/main.dart
+++ lib/main.dart
@@ -19,47 +19,113 @@ class _State extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Battery Powered Heating and Cooling'),
-      ),
+
       body: Container(
-        child: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Switch(
-                value: isSwitched,
-                onChanged: (value) {
-                  setState(() {
-                    isSwitched = value;
-                    print(isSwitched);
-                    if (isSwitched)
-                      Services.sendON();
-                    else
-                      Services.sendOFF();
-                  });
-                },
-                activeTrackColor: Colors.lightGreenAccent,
-                activeColor: Colors.green,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 100, 0, 100),
+              child: Container(
+
+                height: 140,
+                width: 400,
+                decoration: BoxDecoration(
+                    color: Colors.blue[400],
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.all(Radius.circular(5))
+
+                ),
+                child: Center(
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(40, 10, 20, 10),
+                      child: Text('Battery Powered Heating & Cooling Suit',
+                        style: TextStyle(fontSize: 25, color: Colors.white,),),
+                    ),
+                  ),
+
+                ),
               ),
-              RaisedButton(
-                child: Text("Heating"),
-                onPressed: _changeText,
-                color: Colors.red,
-                textColor: Colors.white,
-                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                splashColor: Colors.grey,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.blue,
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.all(Radius.circular(40))
+
               ),
-              RaisedButton(
-                child: Text("Cooling"),
-                onPressed: _changeText,
-                color: Colors.blue,
-                textColor: Colors.white,
-                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                splashColor: Colors.grey,
+              height: 150,
+              width: 300,
+              child: Center(
+                child: Text(
+                  '37',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 60,
+                  ),
+                ),
               ),
-            ],
-          ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 100, 0, 100),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Turn ON/OFF       '),
+                  Transform.scale(
+                    scale: 1.5,
+                    child: Switch(
+                      value: isSwitched,
+                      onChanged: (value) {
+                        setState(() {
+                          isSwitched = value;
+                          print(isSwitched);
+                          if (isSwitched)
+                            Services.sendON();
+                          else
+                            Services.sendOFF();
+                        });
+                      },
+                      activeTrackColor: Colors.lightGreenAccent,
+                      activeColor: Colors.green,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 100,
+                    child: RaisedButton(
+                      child: Text("Heating", style: TextStyle(fontSize: 25),),
+                      onPressed: _changeText,
+                      color: Colors.red,
+                      textColor: Colors.white,
+                      padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                      splashColor: Colors.grey,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    height: 100,
+                    child: RaisedButton(
+                      child: Text("Cooling", style: TextStyle(fontSize: 25),),
+                      onPressed: _changeText,
+                      color: Colors.blue,
+                      textColor: Colors.white,
+                      padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                      splashColor: Colors.grey,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
